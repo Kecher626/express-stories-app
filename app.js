d5dfe853ebbe5f88306b7091c83bae46e7be653a
kecher626 <stichi410714@gmail.com>
2021-07-30 11:37:06 +0800

    setting passport.js config & concat google oauth routes

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
+const passport = require('passport')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 const connectDB = require('./config/db')
 
 // Load config
 dotenv.config({ path: './config/config.env' })
 
+// Passport config
+require('./config/passport')(passport)
+
 
 // -----------------Main process-------------------
 const app = express()
 connectDB()
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Show any http method in the console
 if (process.env.NODE_ENV === 'development') {
@@ -22,12 +30,20 @@ if (process.env.NODE_ENV === 'development') {
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', '.hbs')
 
-// Static folder
-app.use(express.static(path.join(__dirname, 'public')))
+// Sesssion
+app.use(session({
+    secret: 'ctrl v',
+    resave: 'false',
+    saveUninitialized: false
+}))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Routes
 app.use('/', require('./routes/index'))
-
+app.use('/auth', require('./routes/auth'))
 
 
 const PORT = process.env.PORT || 3000
